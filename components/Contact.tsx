ca640bfa8d19d2b1f04a9490b9e8e4407903994b
Chinedu Eseka <esekachinedu256@gmail.com>
2025-03-12 11:47:04 -0700

    add validation to contact form

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -8,6 +8,14 @@ import { Send } from 'lucide-react';
 import { TextArea } from './ui/textarea';
 import emailjs from "@emailjs/browser";
 import { toast } from 'sonner';
+import {useForm} from "react-hook-form"
+import DOMPurify from "dompurify"
+
+type FormFieldType = {
+  name: string;
+  email: string;
+  message: string
+}
 
 const LabelInputContainer = ({
   children,
@@ -26,9 +34,13 @@ const LabelInputContainer = ({
 const Contact = () => {
   const form = useRef<HTMLFormElement | null>(null);
   const [isLoading, setIsLoading] = useState(false)
+    const {
+    register, handleSubmit, setValue, formState: {errors}
+  } = useForm<FormFieldType>()
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+
+  const onSubmit = async () => {
+    // e.preventDefault();
     setIsLoading(true)
 
     if (!form.current) {
@@ -66,18 +78,43 @@ const Contact = () => {
       <h2 className="text-5xl text-center">Get in touch</h2>
 
     <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input mt-8 bg-slate-900 border shadow">
-      <form ref={form} onSubmit={handleSubmit}>
+      <form ref={form} onSubmit={handleSubmit(onSubmit)}>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="name">Name</Label>
-          <Input id="name" name='name' placeholder="John Doe" type="text" />
+          <Input id="name" placeholder="John Doe" type="text" {...register("name", {
+            required: "Please enter your name"
+          })}
+          onBlur={(e) => setValue("name", DOMPurify.sanitize(e.target.value).trim(), { shouldValidate: true })} />
+
+          {errors.name && <p className='text-red-500 text-sm'>{errors.name.message}</p>}
+
         </LabelInputContainer>
+
         <LabelInputContainer className="mb-4">
           <Label htmlFor="email">Email Address</Label>
-          <Input id="email" name='email' placeholder="your@email.com" type="email" />
+          <Input id="email" placeholder="your@email.com" type="email" {...register("email", {
+            required: "Please enter your email",
+            pattern: {
+              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+              message: "Invalid email format"
+
+            }
+          })}
+          onBlur={(e) => setValue("email", DOMPurify.sanitize(e.target.value).trim(), { shouldValidate: true })} />
+
+          {errors.email && <p className='text-red-500 text-sm'>{errors.email.message}</p>}
+
         </LabelInputContainer>
+
         <LabelInputContainer className="mb-4">
           <Label htmlFor="message">Message</Label>
-          <TextArea id='message' name='message' placeholder='your message' rows={5} cols={30} />
+          <TextArea id='message' placeholder='your message' rows={5} cols={30} {...register("message", {
+            required: "Please provide a message"
+          })}
+          onBlur={(e) => setValue("message", DOMPurify.sanitize(e.target.value).trim(), { shouldValidate: true })} />
+
+          {errors.message && <p className='text-red-500 text-sm'>{errors.message.message}</p>}
+
         </LabelInputContainer>
 
         <button 
