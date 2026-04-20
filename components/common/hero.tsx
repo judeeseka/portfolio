b7e9a4deaadf6e48617dc2065ca44ee28cf010bb
Jude Eseka <esekachinedu256@gmail.com>
2026-04-20 17:58:42 +0100

    chore: update resume

--- components/common/hero.tsx
+++ components/common/hero.tsx
@@ -32,7 +32,7 @@ const Hero = () => {
 
             <button className="h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-size-[200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
               <a
-                href="./Jude CV_frontend.pdf"
+                href="./Jude_Eseka_Resume.pdf"
                 download
                 className="flex gap-2 justify-center items-center"
               >
